cf5399550c13de26101e858956e69e5966b28d07
washington.santos <washingtonsilvasan@gmail.com>
2020-02-27 18:22:20 -0300

    add vendas

--- src/app/pages/relatorios/relatorio/relatorio.component.ts
+++ src/app/pages/relatorios/relatorio/relatorio.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../clientes/shared/models/cliente';
 import { RelatorioService } from '../shared/services/relatorio.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Vendedor } from '../../vendedores/shared/models/vendedor';
+import { VendedorService } from '../../vendedores/shared/services/vendedor.service';
 
 @Component({
   selector: 'app-relatorio',
@@ -14,26 +16,44 @@ export class RelatorioComponent implements OnInit {
   clientes: Cliente[] = [];
   cliente: Cliente = null;
   resourceForm: FormGroup;
-
+  vendedores: Vendedor[] = [];
+  vendedor: Vendedor = null;
   customSearchFnCliente: string;
 
   constructor(
     private relatorioService: RelatorioService,
+    private vendedorService: VendedorService,
     private fb: FormBuilder) {
-      this.buildResourceForm();
-   }
+    this.buildResourceForm();
+    this.getVendedores();
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   public buildResourceForm(): void {
     this.resourceForm = this.fb.group({
       dataInicial: ['', [Validators.required]],
       dataFinal: ['', [Validators.required]],
+      vendedor: [this.vendedor]
     });
   }
 
-  gerarRelatorio() {
-    this.relatorioService.gerarPDF();
+  async gerarRelatorio() {
+    return await this.relatorioService.gerarPDF(this.resourceForm.get('vendedor').value);
+  }
+
+  getVendedores() {
+    this.vendedorService.getAll().subscribe(vend => {
+      vend = this.vendedores = vend
+    });
+  }
+
+  async gerarRelatorioVendas() {
+    let gerou = this.gerarRelatorio();
+
+    if (gerou) {
+      alert('Nenhum registro encontrado!')
+    }
   }
 
 }
--- src/app/pages/relatorios/shared/services/relatorio.service.ts
+++ src/app/pages/relatorios/shared/services/relatorio.service.ts
@@ -1,27 +1,24 @@
+import { Vendedor } from 'src/app/pages/vendedores/shared/models/vendedor';
 import { Pedidocabeca } from './../../../pedidos/pedidocabeca/shared/models/pedidocabeca';
 import { Injectable } from '@angular/core';
 import * as jsPDF from 'jspdf';
 import { timer } from 'rxjs';
 
 import { PedidocabecaService } from 'src/app/pages/pedidos/pedidocabeca/shared/services/pedidocabeca.service';
+import { VendedorService } from 'src/app/pages/vendedores/shared/services/vendedor.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RelatorioService {
 
-  constructor(private pedidocabecaService: PedidocabecaService) { }
+  constructor(
+    private pedidocabecaService: PedidocabecaService,
+    private vendedorService: VendedorService) { }
 
   pedidocabeca: Pedidocabeca[] = [];
 
-
-
-  async gerarPDF() {
-
-    // var formatter = new Intl.NumberFormat('pt-BR', {
-    //   style: 'currency',
-    //   currency: 'R$',
-    // });
+  async gerarPDF(vendedor: Vendedor): Promise<Boolean> {
 
     this.pedidocabecaService.getAll().subscribe(s => {
       this.pedidocabeca = s;
@@ -35,80 +32,111 @@ export class RelatorioService {
 
         if (pedido.cliente && pedido.vendedor) {
 
-          documento.setFont("Courier");
-          documento.setFontStyle("bold");
-          documento.setFontSize(20);
-          documento.text("Relatório de Vendas", 65, 15);
+          this.montaCabecalho(documento);
+          this.addLinhaTable(documento, linha, false);
 
-          documento.setFillColor(50, 50, 50);
-
-          documento.rect(10, 20, 30, 8);
-          documento.rect(40, 20, 160, 8);
-          documento.rect(60, 20, 60, 8);
-
-          documento.setFontSize(12);
-          documento.setTextColor(105, 105, 105);
-          documento.text("Data", 15, 25);
-          documento.text("Pedido", 42, 25);
-          documento.text("Cliente", 65, 25);
-          documento.text("Vendedor", 140, 25);
-          documento.text("Total", 185, 25);
-
-          // documento.text("Nome", 12, 33);
-          // documento.text("Preço", 12, 41);
-          documento.setFontStyle("normal");
-          documento.setTextColor(0, 0, 0);
           let dateObj = new Date(pedido.data)
           let dateString = dateObj.toLocaleString('pt', {
             year: 'numeric',
             month: '2-digit',
-            day: '2-digit',
-            // hour: '2-digit',
-            // minute: '2-digit',
-            // second: '2-digit'
+            day: '2-digit'
           })
 
+          documento.setFontStyle("normal");
+          documento.setTextColor(0, 0, 0);
           documento.text(dateString, 12, linha);
           documento.text(String(pedido.id), 42, linha);
           documento.text(pedido.cliente.nome, 62, linha);
-          documento.text(pedido.vendedor.nome, 125, linha);
+          documento.text(pedido.vendedor.nome, 122, linha);
           total += pedido.total;
-
-
-
-          documento.text(String(pedido.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })), 180, linha);
+          documento.text(String(pedido.total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })), 177, linha);
+          linha += 8;
         }
 
         if (linha == 257) {
-          function sleep(time) {
-            return new Promise((resolve) => {
-              setTimeout(resolve, time || 1000);
-            });
-          }
+          this.aguardarGerarPaginaNova();
           numPag += 1;
-          documento.text("pág. " + numPag, 185, linha + 16);
-          documento.output("dataurlnewwindow");
-          documento = new jsPDF();
-          linha = 33;
+          this.finalizaPagina(documento, numPag, linha + 16);
         }
-        else
-          linha += 8;
       });
 
-      documento.setFont("Courier");
-      documento.setFontStyle("bold");
-      documento.text("Total Acumulado:", 120, linha);
-      documento.text(total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }), 180, linha);
-      linha = 290;
+      this.addTotalizador(documento, total, linha);
+      this.finalizaPagina(documento, numPag, linha);
+      return true;
+    });
 
-      if (numPag === 0)
-        numPag = 1;
-
-      documento.text("pág. " + numPag, 185, linha);
-      documento.output("dataurlnewwindow");
-
-    });    //end loop for
+    return false;
   }
 
+  montaCabecalho(documento: jsPDF) {
+
+    documento.setFont("Courier");
+    documento.setFontStyle("bold");
+    documento.setFontSize(20);
+    documento.text("Relatório de Vendas", 65, 15);
+
+    documento.setFillColor(50, 50, 50);
+    documento.rect(10, 20, 30, 8);
+    documento.rect(40, 20, 20, 8);
+    documento.rect(60, 20, 60, 8);
+    documento.rect(120, 20, 55, 8);
+    documento.rect(175, 20, 25, 8);
+
+    documento.setFontSize(10);
+    documento.setFontStyle("bold");
+    // documento.setTextColor(105, 105, 105);
+    documento.text("Data", 12, 25);
+    documento.text("Pedido", 42, 25);
+    documento.text("Cliente", 62, 25);
+    documento.text("Vendedor", 122, 25);
+    documento.text("Total", 177, 25);
+
+  }
+
+  addLinhaTable(documento: jsPDF, linha: number, isTotalizador: boolean) {
+    linha -= 5;
+    documento.setFillColor(50, 50, 50);
+
+    if (!isTotalizador) {
+      documento.rect(10, linha, 30, 8);
+      documento.rect(40, linha, 20, 8);
+      documento.rect(60, linha, 60, 8);
+    }
+    documento.rect(120, linha, 55, 8);
+    documento.rect(175, linha, 25, 8);
+    documento.setFontSize(8);
+    documento.setTextColor(105, 105, 105);
+  }
+
+  finalizaPagina(documento: jsPDF, numPag: number, linha: number) {
+
+    if (numPag === 0)
+      numPag = 1;
+
+    if (linha === 33) {
+      return false;
+    }
+
+    documento.text("pág. " + numPag, 185, 290);
+    documento.output("dataurlnewwindow");
+  }
+
+  aguardarGerarPaginaNova() {
+    function sleep(time) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, time || 1000);
+      });
+    }
+  }
+
+  addTotalizador(documento: jsPDF, total: number, linha: number) {
+    documento.setFont("Courier");
+    documento.setFontStyle("bold");
+    documento.text("Total Acumulado:", 122, linha);
+    documento.text(total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }), 177, linha);
+
+    this.addLinhaTable(documento, linha, true);
+
+  }
 
 }
